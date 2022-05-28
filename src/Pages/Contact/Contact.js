4c009d11e45077b6912cb8e85727329678ff2d2f
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-05-28 18:00:44 +0600

    final commit

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            This is Contact
+        <div className="contact-box my-5">
+            <h5>Address:</h5>
+            <h5>Email: zs.tolon0073@gmail.com</h5>
+            <h5>Phone:</h5>
+            <h5>LinkedIn:</h5>
+            <h5>Facebook:</h5>
+            <h5>Instagram:</h5>
         </div>
     );
 };
